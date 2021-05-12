6acdc2adb2589c4a7f707b3cb49529e35851c57a
emperorlu <emperorlu@hust.edu.cn>
2021-05-12 10:37:01 +0800

    [Debug] Add replica_placement

--- placement.py
+++ placement.py
@@ -2,12 +2,13 @@
 # coding=utf-8
 import park
 from dqn import DQN
+from qlearning import QLearningTable
 
 EPISODE = 10000 # Episode limitation
 STEP = 300 # Step limitation in an episode
 TEST = 10 # The number of experiment test every 100 episode
 
-def main():
+def DQNTest():
 
   env = park.make('replica_placement')
   agent = DQN(env)
@@ -22,9 +23,9 @@ def main():
     while not done:
       action = agent.egreedy_action(state) # e-greedy action for train
       next_state,reward,done = env.step(action)
-      print("state:",state)
-      print("act:",action)
-      print("reward:",reward)
+    #   print("state:",state)
+    #   print("act:",action)
+    #   print("reward:",reward)
       # Define reward for agent
       reward_agent = -1 if done else 0.1
       agent.perceive(state,action,reward,next_state,done)
@@ -45,11 +46,33 @@ def main():
             break
       ave_reward = total_reward/TEST
       print ('episode: ',episode,'Evaluation Average Reward:',ave_reward)
-      if ave_reward >= 200:
+      if ave_reward >= 0:
         break
 
+def QlearningTest():
+    env = park.make('replica_placement')
+    RL = QLearningTable(env.action_space)
+    for episode in range(EPISODE):
+        state = env.reset()
+        done = False
+        while not done:
+            action = RL.choose_action(str(state))
+            state_, reward, done = env.step(action)
+            # RL learn from this transition
+            RL.learn(str(state), action, reward, str(state_))
+            state = state_
+
+    # end of game
+    print('game over')
+    print(RL.q_table)
+    env.destroy()
+
+
 if __name__ == '__main__':
-    main()
+    DQNTest()
+    QlearningTest()
+    
+
 
 
 
@@ -68,3 +91,12 @@ if __name__ == '__main__':
 #         # print("act:\n",act)
 #         print("reward:\n",reward)
 
+
+
+
+if __name__ == "__main__":
+    env = Maze()
+    RL = QLearningTable(actions=list(range(env.n_actions)))
+
+    env.after(100, update)
+    env.mainloop()
