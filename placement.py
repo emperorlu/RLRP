7d8499104ccbcbeb68cbb815a32b1b365499507a
emperorlu <emperorlu@hust.edu.cn>
2021-05-12 15:10:04 +0800

    [Debug] Add replica_placement

--- placement.py
+++ placement.py
@@ -97,8 +97,6 @@ def QlearningTest():
 
     # end of game
     print('game over')
-    print("final_map:",final_map.shape)
-    pg_num = 0
     for pg_num in range(len(final_map)):
         print(pg_num,"———>———>",final_map[pg_num])
     # print(RL.q_table)
