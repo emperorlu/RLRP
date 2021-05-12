53e4926897527410ad0e25ec847cd2aeb316fd88
emperorlu <emperorlu@hust.edu.cn>
2021-05-12 17:01:51 +0800

    [Debug] Add replica_placement

--- park/envs/replica_placement/replica_placement.py
+++ park/envs/replica_placement/replica_placement.py
@@ -66,9 +66,6 @@ class ReplicaplacementEnv(core.Env):
     def r_step(self, actions):
         std1 = np.std(self.servers)
         for action in actions:
-            print("action: ",action)
-            for a in self.action_space:
-                print(a)
             assert self.action_space.contains(action)
             self.servers[action] = self.servers[action] + 1
         std2 = np.std(self.servers)
