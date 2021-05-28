7e21c88e840c4214b99d86dfee85766390997189
emperorlu <emperorlu@hust.edu.cn>
2021-05-28 10:53:08 +0800

    [Debug] Add replica_placement

--- park/envs/replica_placement/replica_placement.py
+++ park/envs/replica_placement/replica_placement.py
@@ -157,7 +157,7 @@ class DatamigrationEnv(core.Env):
 
         self.num_stream_jobs_left = self.num_stream_jobs_left - 1
         done = (self.num_stream_jobs_left == 0)
-        if np.std(self.servers) < 3 and c > 0: done = True
+        if np.std(self.servers) < 2.5: done = True
         if self.servers[-1] >= self.men: done = True
         # if self.servers[-1] == max(self.servers): done = True
         return self.observe(), reward, done
