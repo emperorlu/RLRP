8123c6df6fcffc885f14c56d10a576e8d80f93e2
emperorlu <emperorlu@hust.edu.cn>
2021-06-02 19:43:23 +0800

    [Debug] Add replica_placement

--- placement.py
+++ placement.py
@@ -674,7 +674,7 @@ if __name__ == '__main__':
     # print("begin test\n")
     # QlearningLearn_data()
     # Zhu()
-    DQNLearnSigle()
+    # DQNLearnSigle()
     DQNTestSigle()
     # QlearningLearn()
     # QlearningTest()
