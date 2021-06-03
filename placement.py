4605d91e5ecbb1182a1c18d9da6ab0374718664e
emperorlu <emperorlu@hust.edu.cn>
2021-06-03 17:37:56 +0800

    [Debug] Add replica_placement

--- placement.py
+++ placement.py
@@ -233,6 +233,8 @@ def DQNLearnSigle():
     # Rnum = config.num_rep
     t0 = time.time()
     i = 0
+    testi = 3.5
+    print("testi: ",int(testi))
     for episode in range(EPISODE):
         i += 1
         state = env.reset()
