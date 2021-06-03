3f70be5b5ffa4194bf112cc1e006e2ec350695c3
emperorlu <emperorlu@hust.edu.cn>
2021-06-03 18:37:36 +0800

    [Debug] Add replica_placement

--- park/envs/replica_placement/replica_placement.py
+++ park/envs/replica_placement/replica_placement.py
@@ -44,7 +44,7 @@ class ReplicaplacementEnv(core.Env):
         # for server in self.servers:
         #     server.reset()
         if test==0: self.servers = self.initialize_servers()
-        self.stepn = 1000
+        self.stepn = 400
         self.num_stream_jobs_left = self.num_stream_jobs * config.num_rep
         assert self.num_stream_jobs_left > 0
         return self.observe()
