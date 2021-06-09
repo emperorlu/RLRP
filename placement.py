28f7ace7bb0aba0d70e118383b7aab6022997dfe
emperorlu <emperorlu@hust.edu.cn>
2021-06-09 11:19:20 +0800

    [Debug] Add replica_placement

--- placement.py
+++ placement.py
@@ -509,8 +509,7 @@ def DQN_data():
             agent.perceive(state,action,reward,state_,done)
             state = state_
             fstate = env.observe()
-            # stk = np.std(env.observe_state())
-            stk = -reward
+            stk = np.std(fstate)
             if done:
                 st.append(stk)
                 if stk < equ: 
