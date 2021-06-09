926a44be463ab9ab29b8f41ad43c8099ffc3d593
emperorlu <emperorlu@hust.edu.cn>
2021-06-09 10:57:40 +0800

    [Debug] Add replica_placement

--- park/envs/replica_placement/replica_placement.py
+++ park/envs/replica_placement/replica_placement.py
@@ -179,7 +179,7 @@ class DatamigrationEnv(core.Env):
         
         # std2 = np.std(self.servers)
         # reward = 1000
-        reward = -np.std(state[:,:-1]) **0.5
+        reward = -np.std(state[:-1]) **0.5
         # reward = std1 - std2
         # else: reward -= np.std(self.servers) #* (num+1)
         # reward = (min(self.servers) - max(self.servers)) ** 0.5
