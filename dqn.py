533a44f05fe76cf43b0aa66bf427cc43cffb72c0
emperorlu <emperorlu@hust.edu.cn>
2021-06-10 16:43:35 +0800

    [Debug] Add replica_placement

--- dqn.py
+++ dqn.py
@@ -199,7 +199,7 @@ class DQN():
       Q_action = tf.reduce_sum(tf.multiply(self.Q_value,self.action_input),reduction_indices = 1)
       self.cost = tf.reduce_mean(tf.square(self.y_input - Q_action))
       self.optimizer = tf.compat.v1.train.AdamOptimizer(0.0001).minimize(self.cost)#,var_list=[W1,b1,W2,b2])
-
+      self.session.close()
       self.session.run(tf.compat.v1.global_variables_initializer())
     # print(self.sess.run(W1))  
                 
