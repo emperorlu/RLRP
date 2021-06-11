c0e56622a7d933851d298f67c1d5b2ca327661ae
emperorlu <emperorlu@hust.edu.cn>
2021-06-11 18:31:29 +0800

    [Debug] Add replica_placement

--- placement.py
+++ placement.py
@@ -228,12 +228,12 @@ def DQNLearnSigle3():
 def DQNLearnSigle():
     env = park.make('replica_placement')
     Imodel = 1
-    Ipath = "./dqn_model_1/18.ckpt"
-    based = 17
+    Ipath = "./dqn_model_1/19.ckpt"
+    based = 18
     if Imodel == 0: agent = DQN(env,model=0)
     else:
         agent = DQN(env,model=config.num_servers-based)
-        agent.build_net("./dqn_model_1/17.ckpt",config.num_servers-based)
+        agent.build_net("./dqn_model_1/18.ckpt",config.num_servers-based)
     # agent = DQN(env,1,"./dqn_model/place.ckpt")#,0.1)
     e = EPISODE / 10
     equ = 100
