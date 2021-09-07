f85d76c7c3ecd73463da4654ded451c6e069eb26
emperorlu <emperorlu@hust.edu.cn>
2021-09-07 15:09:33 +0800

    [Debug] Change replica_placement

--- dqn.py
+++ dqn.py
@@ -122,7 +122,7 @@ class DQN():
       while next:
         x[np.argmax(x)] = np.min(x)
         next = next - 1
-      print("np.argmax(x): ", np.argmax(x))
+      # print("np.argmax(x): ", np.argmax(x))
       return np.argmax(x)
 
     
