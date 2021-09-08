87c9a2ba7d927e8a9dcace78d2af0cfb370b8d55
emperorlu <emperorlu@hust.edu.cn>
2021-09-08 16:49:10 +0800

    [Debug] Change replica_placement

--- placement.py
+++ placement.py
@@ -432,7 +432,7 @@ def DQNTestSigle():
     for pg_num in range(len(final_map)):
         print(pg_num,"————>",final_map[pg_num], file=f)
         for k in (final_map[pg_num]):
-            print(hstate[k]," state:hash ", Hash[pg_num], file=f)
+            # print(hstate[k]," state:hash ", Hash[pg_num], file=f)
             hstate[k] = hstate[k] + Hash[pg_num]
         # mapping[pg_num][final_map[pg_num]] = 1
     print("\nservers:", hstate)
